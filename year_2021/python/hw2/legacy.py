97e705a801f84400a585ac848b9782d058dcdd43
Konsantin <92954470+Konsantin@users.noreply.github.com>
2021-11-02 19:16:37 +0300

    Update legacy.py

--- year_2021/python/hw2/legacy.py
+++ year_2021/python/hw2/legacy.py
@@ -1,18 +1,26 @@
-def t1(number):
-    """
-    Поправьте код, чтобы возвращаемое значение было ближайшим сверху, кратным к 20
-    Пример: number=21 тогда нужно вернуть 40
-    Пример: -5 -> 0
+  Пример: -5 -> 0
     """
     pass
-
-
+    if number % 20 == 0:
+        return number
+    else:
+        if number < 0:
+            return (int(number/20)*20)
+        else:
+            return (int(number/20)*20 + 20)
+            
+            
+            
 def t2(string):
-    """
-    На вход подается набор слов, разделенных пробелом, инвертируйте каждое слово.
+	
     Пример: `abc abc abc` -> `cba cba cba`
     """
     pass
+    lst = []
+    str_splitted = string.split(' ')
+    for i in str_splitted:
+        lst.append(i[::-1])
+    return (' '.join(lst))
 
 
 def t3(dictionary):
@@ -20,6 +28,16 @@ def t3(dictionary):
     На вход подается словарь. Преобразуйте его в строку по следующему шаблону 'key: value; key: value' и так далее
     """
     pass
+    str_new = ''
+    k = 0
+
+    for key, value in dictionary.items():
+        if k == len(dictionary) - 1:
+            str_new = str_new + f'{key}: {value}'
+        else:
+            str_new = str_new + f'{key}: {value}; '
+            k += 1
+    return str_new
 
 
 def t4(string, sub_string):
@@ -27,6 +45,10 @@ def t4(string, sub_string):
     проверить есть ли в строке инвертированная подстрока
     """
     pass
+    if string[::-1].find(sub_string) != -1:
+        return True
+    else:
+        return False
 
 
 def t5(strings):
